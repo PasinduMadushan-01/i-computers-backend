e0bc58c916fadd69cff6edd9ce5762be7de42869
pasindu <madushanpasindu091@gmail.com>
2026-04-27 06:41:48 +0530

    tailwindcss add

--- i-computer-fontend/src/components/productCard.jsx
+++ i-computer-fontend/src/components/productCard.jsx
@@ -2,8 +2,8 @@ export default function ProductCard(props){
  
   
     return(
-        <div>
-            <h1>{props.name}</h1>
+        <div className="bg-red-400 border w-100" >
+            <h1 className="text-white">{props.name}</h1>
             <img src={props.image} alt="random image" />
             <p>{props.price}</p>
             <button>buy now</button>
